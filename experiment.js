9dc78a6d87aec049d8b979eb62d5a48ed96de7ed
b18veran <49142649+b18veran@users.noreply.github.com>
2021-03-07 11:20:26 +0100

    Added a counter and gets the time it takes to load the page.

--- experiment.js
+++ experiment.js
@@ -1,5 +1,26 @@
+var startTime = new Date().getTime();
+var counter = parseInt(localStorage.getItem("Counter", 0));
+
 function startUp() {
 
+    // Om countern är null sätts den till 0. 
+    if (localStorage.getItem("Counter") == null) {
+        localStorage.setItem("Counter", 0);
+    }
+    // Ser till att den itereras endast 5 gånger.
+    if (counter <= 4) {
+        var endTime = new Date().getTime();
+        var theTime = endTime - startTime;
+        endTime = localStorage.setItem("Tid", JSON.stringify(theTime));
+        counter++;
+        readMore();
+        logo();
+        localStorage.getItem(startTime);
+
+
+    }
+}
+function readMore() {
     let donePageLoading = document.readyState; //NOTE! Denna ser till att allt laddats in innan den går vidare.
     //Klickar på knappen med id readMore
     var clickEvent = new MouseEvent('click', {
@@ -8,4 +29,15 @@ function startUp() {
         cancelable: true
     });
     document.getElementById("readMore").dispatchEvent(clickEvent);
+    localStorage.setItem('Counter', counter);
+}
+function logo() {
+    let donePageLoading = document.readyState;
+    //Klickar på knappen med id logo
+    var clickEvent = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
+    document.getElementById("logo").dispatchEvent(clickEvent);
 }
