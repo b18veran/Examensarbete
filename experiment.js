0ab770a06ffce709e0165d09df75bc9bd48a40e1
b18veran <49142649+b18veran@users.noreply.github.com>
2021-03-07 11:13:46 +0100

    Added function that click a button when all styling has been done loading.

--- experiment.js
+++ experiment.js
@@ -1,4 +1,11 @@
 function startUp() {
 
+    let donePageLoading = document.readyState; //NOTE! Denna ser till att allt laddats in innan den går vidare.
+    //Klickar på knappen med id readMore
+    var clickEvent = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
+    document.getElementById("readMore").dispatchEvent(clickEvent);
 }
-
