a6b9fde2128bf5115a61bc8eb606e0338d263cc8
b18veran <49142649+b18veran@users.noreply.github.com>
2021-03-09 11:16:10 +0100

    Added shots & milkshake products. (Bootstrap)

--- navigation.js
+++ navigation.js
@@ -1,52 +1,80 @@
 /*_____________________________________Byt sida funktioner________________________________*/
-function showStart(){
+function showStart() {
     document.getElementById("start").style.display = "block";
     document.getElementById("productInfo").style.display = "none";
     document.getElementById("productsJuice").style.display = "none";
+    document.getElementById("productsShot").style.display = "none";
     document.getElementById("productsSmoothie").style.display = "none";
-}
-function showProductInfo(){
+    document.getElementById("productsMilkshake").style.display = "none";
+  }
+  function showProductInfo() {
     document.getElementById("start").style.display = "none";
     document.getElementById("productInfo").style.display = "block";
     document.getElementById("productsJuice").style.display = "none";
+    document.getElementById("productsShot").style.display = "none";
     document.getElementById("productsSmoothie").style.display = "none";
-}
-function showProductsJuice(){
+    document.getElementById("productsMilkshake").style.display = "none";
+  }
+  function showProductsJuice() {
     document.getElementById("start").style.display = "none";
     document.getElementById("productInfo").style.display = "none";
     document.getElementById("productsJuice").style.display = "block";
+    document.getElementById("productsShot").style.display = "none";
     document.getElementById("productsSmoothie").style.display = "none";
-}
-function showProductsSmoothie(){
-  document.getElementById("start").style.display = "none";
-  document.getElementById("productInfo").style.display = "none";
-  document.getElementById("productsJuice").style.display = "none";
-  document.getElementById("productsSmoothie").style.display = "block";
-}
-
-
-
-/*_____________________________________Historik funktion________________________________*/
-
-function setupHistory(){
-  window.onpopstate = function(event) { historyChange(event); };
-}
-
-function historyChange(event){
-  document.getElementById("outputValue").innerHTML+=JSON.stringify(event.state);
-  if(event.state=="start"){
-    showStart();
-  }else if (event.state=="productInfo") {
-    showProductInfo();
-  }else if (event.state=="productsJuice") {
-    showProductsJuice();
-  }else if (event.state=="productsSmoothies") {
-    showProductsSmoothie();
-  }else{
-
+    document.getElementById("productsMilkshake").style.display = "none";
   }
-}
-function updateHistory(token){
-  history.pushState(token, "Titel: "+token);
-  console.log("sparat historik",token);
-}
+  function showProductsShot() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("productInfo").style.display = "none";
+    document.getElementById("productsJuice").style.display = "none";
+    document.getElementById("productsShot").style.display = "block";
+    document.getElementById("productsSmoothie").style.display = "none";
+    document.getElementById("productsMilkshake").style.display = "none";
+  }
+  function showProductsSmoothie() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("productInfo").style.display = "none";
+    document.getElementById("productsJuice").style.display = "none";
+    document.getElementById("productsShot").style.display = "none";
+    document.getElementById("productsSmoothie").style.display = "block";
+    document.getElementById("productsMilkshake").style.display = "none";
+  }
+  function showProductsMilkshake() {
+    document.getElementById("start").style.display = "none";
+    document.getElementById("productInfo").style.display = "none";
+    document.getElementById("productsJuice").style.display = "none";
+    document.getElementById("productsShot").style.display = "none";
+    document.getElementById("productsSmoothie").style.display = "none";
+    document.getElementById("productsMilkshake").style.display = "block";
+  }
+  
+  
+  
+  /*_____________________________________Historik funktion________________________________*/
+  
+  function setupHistory() {
+    window.onpopstate = function (event) { historyChange(event); };
+  }
+  
+  function historyChange(event) {
+    document.getElementById("outputValue").innerHTML += JSON.stringify(event.state);
+    if (event.state == "start") {
+      showStart();
+    } else if (event.state == "productInfo") {
+      showProductInfo();
+    } else if (event.state == "productsJuice") {
+      showProductsJuice();
+    } else if (event.state == "productsSmoothie") {
+      showProductsSmoothie();
+    }else if (event.state == "productsShot") {
+      showProductsShot();
+    }else if (event.state == "productsMilkshake") {
+      showProductsMilkshake();
+    } else {
+  
+    }
+  }
+  function updateHistory(token) {
+    history.pushState(token, "Titel: " + token);
+    console.log("sparat historik", token);
+  }
