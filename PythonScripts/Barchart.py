59f94079a44cd83318afae60b368e604657ab7df
b18veran <49142649+b18veran@users.noreply.github.com>
2021-03-15 13:09:02 +0100

    Created Barchart.py

---
+++ PythonScripts/Barchart.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def exampleBarChars():
+
+
+    #Read your data from file
+    file = "data"
+    df = pd.read_csv(file, sep=",", header=None, names=['Bootstrap','Foundation'])
+
+
+    # width of the bars
+    barWidth = 0.6
+
+    # Bars Data
+    barsData = df.mean()
+
+    # The x-position order of bars
+    barsOrder = range(len(df.columns))
+
+    # Std Bars Interval
+    barsInterval = df.std()
+
+    # Standard Error Bars intervals
+    #barsInterval = df.sem()
+
+    # Colours of bar charts
+    colors=["cyan", "purple"]
+
+    # Opacity of colours
+    Opacity=0.5
+
+    # Start values from bottom of the bars
+    minValue=df.values.min()
+
+    # Interval cap size
+    intervalCapsize=7
+
+    # Plot bars
+    plt.bar(barsOrder, barsData, color = colors , edgecolor = 'black', width = barWidth,
+        yerr=barsInterval, capsize=7, alpha=Opacity, bottom=intervalCapsize)
+
+    #Put a tick on the x-axis undex each bar and label it with column name
+    plt.xticks(range(len(df.columns)), df.columns)
+
+    plt.ylabel('Load Time In Milliseconds')
+    #plt.xlabel('Browsers')
+    plt.title('Bar-chart')
+    plt.show()
+
+exampleBarChars()
