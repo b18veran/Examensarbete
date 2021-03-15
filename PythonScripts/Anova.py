bd40fb3e43d5ba7b9440e79f4e2d0bae09367c1c
b18veran <49142649+b18veran@users.noreply.github.com>
2021-03-15 13:07:14 +0100

    Added Anova.py

---
+++ PythonScripts/Anova.py
@@ -0,0 +1,39 @@
+import scipy.stats as stats
+import numpy as np
+import statsmodels.stats.multicomp as multi
+import pandas as pd
+
+
+def anova(*data):  # * indicates, 0, 1 , 2 .. arguments
+
+    if len(data) == 2:
+        statistic, pvalue = stats.f_oneway(data[0], data[1])
+    elif len(data) == 3:
+        statistic, pvalue = stats.f_oneway(data[0], data[1], data[2])
+    elif len(data) == 4:
+        statistic, pvalue = stats.f_oneway(data[0], data[1], data[2], data[3])
+
+    print("ANOVA Statistic " + str(statistic) + " and p-value " + str(pvalue))
+
+    if pvalue < statistic:
+        return True
+    else:
+        return False
+
+
+def exampleAnova():
+    # Determine the group labels
+    browsers = ['Bootstrap', 'Foundation']
+
+    # Read your data from file
+    file = "data"
+    df = pd.read_csv(file, sep=",", header=None, names=['Bootstrap', 'Foundation'])
+
+    # Run Anova on data groups
+    if (anova(df['Bootstrap'], df['Foundation'])):
+        print("The means are different")
+    else:
+        print("No differences in means")
+
+
+exampleAnova()
